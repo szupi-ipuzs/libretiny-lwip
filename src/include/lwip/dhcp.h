d0dee361eb97badd54e5bef948966fbe4fb60bb0
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2022-08-25 22:27:50 +0200

    [LT] Add vendor changes from bdk_freertos/v2.0.2 (opts)

--- src/include/lwip/dhcp.h
+++ src/include/lwip/dhcp.h
@@ -67,7 +67,7 @@ typedef u16_t dhcp_timeout_t;
 /** period (in milliseconds) of the application calling dhcp_coarse_tmr() */
 #define DHCP_COARSE_TIMER_MSECS (DHCP_COARSE_TIMER_SECS * 1000UL)
 /** period (in milliseconds) of the application calling dhcp_fine_tmr() */
-#define DHCP_FINE_TIMER_MSECS   500
+#define DHCP_FINE_TIMER_MSECS   100
 
 #define DHCP_BOOT_FILE_LEN      128U
 
